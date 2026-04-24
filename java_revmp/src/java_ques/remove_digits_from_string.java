c64aabbe578e811037d63cd13a3f9b487864fe12
anuragsharma1098 <anuragsharma1098@gmail.com>
2026-04-24 14:01:52 +0530

    remove digit from string

---
+++ java_revmp/src/java_ques/remove_digits_from_string.java
@@ -0,0 +1,19 @@
+package java_ques;
+
+public class remove_digits_from_string {
+    public static void main(String[] args) {
+        String str = "a1b2c3d4e5f6g7h8i9j0";
+        String result = removeDigits(str);
+        System.out.println(result); // Output: abcdefghij
+    }
+
+    public static String removeDigits(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            if (!Character.isDigit(ch)) {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
